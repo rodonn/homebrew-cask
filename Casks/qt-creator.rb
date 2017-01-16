9889663acd04ffe568d46dfc038cb988eab2ca12
Rob Donnelly <rndonnelly@gmail.com>
2017-01-15 22:58:37 -0800

    Update qt-creator to 5.7.1

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,8 +1,8 @@
 cask 'qt-creator' do
-  version '4.2.0'
-  sha256 '5b9d7fda77b6d3e98bb65d1fb7a33552f4a2e0e8533930c6acc0fbbbeb60dc21'
+  version '5.7.1'
+  sha256 'c5b17b967aa0d31c0de91681c7ad824f4187141b12d2b0ce78b17a2accce6ed6'
 
-  url "http://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
+  url "http://download.qt.io/official_releases/qt/#{version.major_minor}/#{version}/qt-opensource-mac-x64-clang-#{version}.dmg"
   name 'Qt Creator'
   homepage 'https://www.qt.io/developers/'
 
